5535853691da2e573b6c9a661ca434ca54be747c
AnkitDhayal <149352781+AnkitDhayal@users.noreply.github.com>
2023-12-20 10:59:30 +0530

    Delete View-Sphere-Implementation directory

--- View-Sphere-Implementation/TypeDef.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<vector>
-#include"Triangle.h"
-#include"Point3D.h"
-
-typedef std::vector<SphereView::Point3D> PointList;
-typedef std::vector<SphereView::Triangle> TriangleList;
